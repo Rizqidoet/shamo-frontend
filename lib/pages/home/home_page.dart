18dd76ad15853bc74ab90080dc092b52e7e6f31e
rizqidoet <muhamadrizqifadila@gmail.com>
2021-12-22 15:13:59 +0700

    update ui an fixing function all pages

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,7 +4,9 @@ import 'package:bwa_shamo/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  // const HomePage({Key? key}) : super(key: key);
+
+  int sekundren = 0;
 
   Widget header() {
     return Container(
@@ -43,7 +45,7 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget categories() {
+  Widget categories(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 5, bottom: 5, right: defaultMargin),
       margin: EdgeInsets.only(top: defaultMargin),
@@ -54,53 +56,91 @@ class HomePage extends StatelessWidget {
             SizedBox(
               width: defaultMargin,
             ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-              decoration: BoxDecoration(
-                  color: primaryColor, borderRadius: BorderRadius.circular(12)),
-              child: Text(
-                "All shoes",
-                style:
-                    primaryTextStyle.copyWith(fontSize: 13, fontWeight: medium),
+            GestureDetector(
+              onTap: () {
+                sekundren = 0;
+                print(sekundren);
+                (context as Element).reassemble();
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                decoration: BoxDecoration(
+                    color: sekundren == 0 ? primaryColor : Colors.transparent,
+                    border: sekundren == 0
+                        ? null
+                        : Border.all(color: subtitleTextColor),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Text(
+                  "All shoes",
+                  style: primaryTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 16),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-              decoration: BoxDecoration(
-                  color: transaparentColor,
-                  border: Border.all(color: subtitleTextColor),
-                  borderRadius: BorderRadius.circular(12)),
-              child: Text(
-                "Running",
-                style: subtitleTextStyle.copyWith(
-                    fontSize: 13, fontWeight: medium),
+            GestureDetector(
+              onTap: () {
+                sekundren = 1;
+                print(sekundren);
+                (context as Element).reassemble();
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 16),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                decoration: BoxDecoration(
+                    color: sekundren == 1 ? primaryColor : Colors.transparent,
+                    border: sekundren == 1
+                        ? null
+                        : Border.all(color: subtitleTextColor),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Text(
+                  "Running",
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 16),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-              decoration: BoxDecoration(
-                  color: transaparentColor,
-                  border: Border.all(color: subtitleTextColor),
-                  borderRadius: BorderRadius.circular(12)),
-              child: Text(
-                "Training",
-                style: subtitleTextStyle.copyWith(
-                    fontSize: 13, fontWeight: medium),
+            GestureDetector(
+              onTap: () {
+                sekundren = 2;
+                print(sekundren);
+                (context as Element).reassemble();
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 16),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                decoration: BoxDecoration(
+                    color: sekundren == 2 ? primaryColor : Colors.transparent,
+                    border: sekundren == 2
+                        ? null
+                        : Border.all(color: subtitleTextColor),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Text(
+                  "Training",
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 16),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-              decoration: BoxDecoration(
-                  color: transaparentColor,
-                  border: Border.all(color: subtitleTextColor),
-                  borderRadius: BorderRadius.circular(12)),
-              child: Text(
-                "Basketball",
-                style: subtitleTextStyle.copyWith(
-                    fontSize: 13, fontWeight: medium),
+            GestureDetector(
+              onTap: () {
+                sekundren = 3;
+                print(sekundren);
+                (context as Element).reassemble();
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 16),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                decoration: BoxDecoration(
+                    color: sekundren == 3 ? primaryColor : Colors.transparent,
+                    border: sekundren == 3
+                        ? null
+                        : Border.all(color: subtitleTextColor),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Text(
+                  "Basketball",
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
               ),
             ),
             Container(
@@ -122,6 +162,285 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Widget allShoes() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, right: defaultMargin, left: defaultMargin),
+            child: Text(
+              "Popular products",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 14, left: defaultMargin),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ProductCard(
+                    imageProduct: 'assets/images/runningDepan-1.png',
+                    categoryProduct: 'runing',
+                    titleProduct: "Aerostreet Hoops Gum Series",
+                    priceProduct: "\$23.32",
+                  ),
+                  ProductCard(
+                    imageProduct: 'assets/images/basketDepan-1.png',
+                    categoryProduct: 'basket',
+                    titleProduct: "Aerostreet Hoops 2D Series",
+                    priceProduct: "\$23.32",
+                  ),
+                  ProductCard(
+                    imageProduct: 'assets/images/hikingDepan-1.png',
+                    categoryProduct: 'hiking',
+                    titleProduct: "Aerostreet Hoops Low Series",
+                    priceProduct: "\$23.32",
+                  ),
+                  ProductCard(
+                    imageProduct: 'assets/images/trainingDepan-1.png',
+                    categoryProduct: 'training',
+                    titleProduct: "Aerostreet Hoops x NamaKalian",
+                    priceProduct: "\$23.32",
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, right: defaultMargin, left: defaultMargin),
+            child: Text(
+              "New Arrivals",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: 14,
+                left: defaultMargin,
+                bottom: defaultMargin,
+                right: defaultMargin),
+            child: Column(
+              children: [
+                NewArrivalCard(
+                  imageProduct: 'assets/images/runningDepan-1.png',
+                  categoryProduct: 'runing',
+                  titleProduct: "Aerostreet Hoops Gum Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/basketDepan-1.png',
+                  categoryProduct: 'basket',
+                  titleProduct: "Aerostreet Hoops 2D Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/hikingDepan-1.png',
+                  categoryProduct: 'hiking',
+                  titleProduct: "Aerostreet Hoops Low Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/trainingDepan-1.png',
+                  categoryProduct: 'training',
+                  titleProduct: "Aerostreet Hoops x NamaKalian",
+                  priceProduct: "\$23.32",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget runing() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, right: defaultMargin, left: defaultMargin),
+            child: Text(
+              "Runing",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: 14,
+                left: defaultMargin,
+                bottom: defaultMargin,
+                right: defaultMargin),
+            child: Column(
+              children: [
+                NewArrivalCard(
+                  imageProduct: 'assets/images/runningDepan-1.png',
+                  categoryProduct: 'runing',
+                  titleProduct: "Aerostreet Hoops Gum Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/runningDepan-2.png',
+                  categoryProduct: 'basket',
+                  titleProduct: "Aerostreet Hoops 2D Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/runningDepan-3.png',
+                  categoryProduct: 'hiking',
+                  titleProduct: "Aerostreet Hoops Low Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/runningDepan-4.png',
+                  categoryProduct: 'training',
+                  titleProduct: "Aerostreet Hoops x NamaKalian",
+                  priceProduct: "\$23.32",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget training() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, right: defaultMargin, left: defaultMargin),
+            child: Text(
+              "Training",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: 14,
+                left: defaultMargin,
+                bottom: defaultMargin,
+                right: defaultMargin),
+            child: Column(
+              children: [
+                NewArrivalCard(
+                  imageProduct: 'assets/images/trainingDepan-1.png',
+                  categoryProduct: 'runing',
+                  titleProduct: "Aerostreet Hoops Gum Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/trainingDepan-2.png',
+                  categoryProduct: 'basket',
+                  titleProduct: "Aerostreet Hoops 2D Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/trainingDepan-3.png',
+                  categoryProduct: 'hiking',
+                  titleProduct: "Aerostreet Hoops Low Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/trainingDepan-4.png',
+                  categoryProduct: 'training',
+                  titleProduct: "Aerostreet Hoops x NamaKalian",
+                  priceProduct: "\$23.32",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget basketBall() {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+                top: defaultMargin, right: defaultMargin, left: defaultMargin),
+            child: Text(
+              "Basket Ball",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+                top: 14,
+                left: defaultMargin,
+                bottom: defaultMargin,
+                right: defaultMargin),
+            child: Column(
+              children: [
+                NewArrivalCard(
+                  imageProduct: 'assets/images/basketDepan-1.png',
+                  categoryProduct: 'runing',
+                  titleProduct: "Aerostreet Hoops Gum Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/basketDepan-2.png',
+                  categoryProduct: 'basket',
+                  titleProduct: "Aerostreet Hoops 2D Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/basketDepan-3.png',
+                  categoryProduct: 'hiking',
+                  titleProduct: "Aerostreet Hoops Low Series",
+                  priceProduct: "\$23.32",
+                ),
+                NewArrivalCard(
+                  imageProduct: 'assets/images/basketDepan-4.png',
+                  categoryProduct: 'training',
+                  titleProduct: "Aerostreet Hoops x NamaKalian",
+                  priceProduct: "\$23.32",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget categoryMenu() {
+    switch (sekundren) {
+      case 0:
+        return allShoes();
+        break;
+      case 1:
+        return runing();
+        break;
+      case 2:
+        return training();
+        break;
+      case 3:
+        return basketBall();
+        break;
+      default:
+        return allShoes();
+    }
+  }
+
+// Widget Gak Kepake tp lumayan buat contekan
   Widget popularProdcutTitle() {
     return Container(
       margin: EdgeInsets.only(
@@ -219,17 +538,11 @@ class HomePage extends StatelessWidget {
     );
   }
 
+// Widget Gak Kepake tp lumayan buat contekan
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [
-        header(),
-        categories(),
-        popularProdcutTitle(),
-        popularProducts(),
-        newArrivalTitle(),
-        newArrivals()
-      ],
+      children: [header(), categories(context), categoryMenu()],
     );
   }
 }
